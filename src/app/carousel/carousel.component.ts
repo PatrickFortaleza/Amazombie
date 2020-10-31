ebce5dee3f402cf5b62721bc654d45acc1b65eb0
Patrick Fortaleza <patrickfortaleza@gmail.com>
2020-10-31 10:11:37 -0700

    add carousel nav functionality

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,39 +1,57 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, ChangeDetectorRef } from '@angular/core';
+import { SlideComponent } from './slide/slide.component';
+import { Slide } from './slide.model';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss']
 })
-export class CarouselComponent implements AfterViewInit {
+export class CarouselComponent implements AfterViewInit{
+  // querySelect carouseltrack
   @ViewChild('carouseltrack') ul: ElementRef
+  @ViewChild('carouselnav') div: ElementRef
+  @ViewChild(SlideComponent) child;
 
+  // Initiate state variables
   track: HTMLElement = null;
+  carouselnav: HTMLElement = null;
+  slides: Slide [] = [];
   currentSlide: HTMLElement = null;
   nextSlide: HTMLElement = null;
-  allSlides: Array<HTMLElement>;
-  
+  currentDot: HTMLElement = null;
+
   ngAfterViewInit(){
     this.getCurrentSlide();
     this.track = this.ul.nativeElement as HTMLElement;
-    let slides: Array<HTMLElement> = Array.from(this.ul.nativeElement.children[0].children);
-    this.allSlides = slides;
+    this.carouselnav = this.div.nativeElement as HTMLElement;
+    this.slides = this.child.slides
+    this.cdr.detectChanges();
   }
 
+  // Sets the current slide object
   getCurrentSlide(){
+    // Find all slides from track
     let slides: Array<HTMLElement> = Array.from(this.ul.nativeElement.children[0].children)
+    // Find slide with class current-slide
     let result: Array<HTMLElement> = slides.filter((slide) => {
       if(slide.classList.contains('current-slide')){
         return slide;
       }
     });
+    // Attach result to currentSlide state variable
     this.currentSlide = result[0];
   }
 
+  // Moves slide left or right
   moveToSlide(track, current, next){
-    track.style.transform = `translateX(-${this.nextSlide.style.left})`
+    // based on the next slide, translate the track to the correct position
+    track.style.transform = `translateX(-${next.style.left})`
+    // Remove current slide class from current slide
     current.classList.remove('current-slide');
+    // The next slide becomes the current-slide
     next.classList.add('current-slide');
+    // Save the next and current class in state
     this.getCurrentSlide();
   }
 
@@ -61,20 +79,35 @@ export class CarouselComponent implements AfterViewInit {
     this.moveToSlide(this.track, this.currentSlide, this.nextSlide);
   }
 
-  onResize($event){
+  navClick($event){
+    let targetDot: HTMLElement = $event.target.closest('button')
+    if(!targetDot) return null;
+    let slides: Array<HTMLElement> = Array.from(this.ul.nativeElement.children[0].children)
+    let dots: Array<HTMLElement> = Array.from(this.div.nativeElement.children)
+    let result: Array<HTMLElement> = dots.filter((dot) => {
+      if(dot.classList.contains('current-slide')){
+        return dot;
+      }
+    });
+    this.currentDot = result[0];
+
+    let targetIndex = dots.findIndex(dot => dot == targetDot);
+    let targetSlide: HTMLElement = slides[targetIndex];
+    this.moveToSlide(this.track, this.currentSlide, targetSlide);
+  }
+
+  // detect windor resizing and reposition track
+  onResize(){
     console.log('resizing');
     setTimeout(() => {
       this.setTrackPosition(this.currentSlide);
     }, 500)
   }
 
+  // sets track position - for responsivensss
   setTrackPosition(slide){
     this.track.style.transform = `translateX(-${slide.style.left})`;
   }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor(private cdr: ChangeDetectorRef) { }
 }
